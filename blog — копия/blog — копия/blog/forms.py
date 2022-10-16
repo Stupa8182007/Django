3c85b029ca69a4fbf7bc72552db1fabee0a4475b
Stupa8182007 <63909938+Stupa8182007@users.noreply.github.com>
2022-10-16 15:20:45 +0200

    Delete blog — копия directory

--- blog — копия/blog — копия/blog/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import QuillPost
-
-class QuillPostForm(forms.ModelForm):
-    class Meta:
-        model = QuillPost
-        fields = (
-            'content',
-        )
